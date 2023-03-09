4a3138ba5360e98aab506de26445cd11ba223e5a
houdamzari <houda.mzari1999@gmail.com>
2023-03-09 09:04:14 +0100

    Fix linter issues

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
   def correct_name
-    raise NotImplementedError("Subclass must implement abstract method")
+    raise NotImplementedError('Subclass must implement abstract method')
   end
 end
