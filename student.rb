0403b1c657e3096ca2e1fbbb72dba37fa0be706f
houdamzari <houda.mzari1999@gmail.com>
2023-03-15 12:46:36 +0100

    Fix linter issues

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age,name, parent_permission: true, classroom: nil)
+  def initialize(age, name, parent_permission: true, classroom: nil)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
