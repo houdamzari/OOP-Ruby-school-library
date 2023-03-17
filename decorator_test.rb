d9c990aea413d09efbe844e0f41bfa7011820979
kennedyowusu <kennediowusu@gmail.com>
2023-03-17 12:32:27 +0000

    write test for decorator

---
+++ decorator_test.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative 'decorator'
+
+describe Decorator do
+  let(:nameable) { double('nameable') }
+  let(:decorator) { described_class.new(nameable) }
+
+  describe '#correct_name' do
+    it 'calls correct_name on the wrapped object' do
+      expect(nameable).to receive(:correct_name)
+      decorator.correct_name
+    end
+  end
+end
