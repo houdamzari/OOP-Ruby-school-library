61a4e1180d86e1d93448e0266277f54b76812cbe
kennedyowusu <kennediowusu@gmail.com>
2023-03-17 15:14:03 +0000

    change the file path in the book test class

--- spec/book_test.rb
+++ spec/book_test.rb
@@ -1,6 +1,6 @@
-require_relative 'book'
-require_relative 'rental'
-require_relative 'person'
+require_relative '../book'
+require_relative '../rental'
+require_relative '../person'
 require 'rspec'
 
 describe Book do
